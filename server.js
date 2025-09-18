ec8334c247dbb868eddb2fc69914ee6cb2e09a06
lemoapp <biggyassets@gmail.com>
2025-09-18 17:35:24 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1494,7 +1494,9 @@ pool.getConnection((err, connection) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
